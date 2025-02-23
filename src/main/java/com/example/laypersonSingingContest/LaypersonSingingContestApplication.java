db495f58f6d287dc5a861b71c4228899215f3e35
choiSangHyeop <xoxoxmin1268@naver.com>
2025-02-24 01:17:55 +0900

    환경ë설정완료 by상협

---
+++ src/main/java/com/example/laypersonSingingContest/LaypersonSingingContestApplication.java
@@ -0,0 +1,13 @@
+package com.example.laypersonSingingContest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaypersonSingingContestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaypersonSingingContestApplication.class, args);
+	}
+
+}
