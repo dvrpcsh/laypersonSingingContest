7a287c49b57b6707d1cdd03750a42765a2ed4993
choiSangHyeop <xoxoxmin1268@naver.com>
2025-03-25 00:28:21 +0900

    cors policy complete and progress bar

--- react-study/survey-pie/src/components/ActionButtons/index.js
+++ react-study/survey-pie/src/components/ActionButtons/index.js
@@ -1,36 +1,72 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+
 import Button from '../Button';
 import styled from 'styled-components';
 import questionsLengthState from '../../globalVariable/survey/questionsLengthState';
-import { useRecoilValue } from 'recoil';
 import useStep from '../../hooks/useStep';
 import useSurveyId from '../../hooks/useSurveyId';
 import postAnswers from '../../services/postAnswers';
 import useAnswers from '../../hooks/useAnswers';
 
+import useRequiredOption from '../../hooks/useRequiredOption';
+
 function ActionButtons () {
     const step = useStep();
     const surveyId = useSurveyId();
-    const questionsLength = useRecoilValue(questionsLengthState);
-    const answers = useAnswers();
 
-    const isLast= questionsLength-1 === step;
+    const questionsLength = useRecoilValue(questionsLengthState);
+    const [answers, setAnswers] = useAnswers();
+    const [isPosting, setIsPosting] = useState(false);
+    const isRequired = useRequiredOption();
+
     const navigate = useNavigate();
+    const isLast= questionsLength-1 === step;
+    const isBlockToNext = isRequired ? !answers[step]?.length : false;
+
+
 
 
     return (
     <ActionButtonsWrapper>
-        {step === 0 || <Button type="TERTIARY" onClick={() => {navigate(`${step-1}`)}}>이전</Button>}
-        {isLast ? <Button type="PRIMARY"
-                          onClick={() => {
-                            postAnswers(surveyId, answers);
-                            navigate('/done');
-                          }}
-                  >제출</Button> :
-                  <Button type="PRIMARY" onClick={() => {navigate(`${step+1}`)}}>다음</Button>}
-
+        {step === 0 || (
+            <Button
+              type="TERTIARY"
+              onClick={() => {
+                navigate(`${step-1}`);
+                }}>이전</Button>
+        )}
+        {isLast ? (<Button
+                     type="PRIMARY"
+                     onClick={() => {
+                        setIsPosting(true);
+                        postAnswers(surveyId, answers)
+                            .then(() => {
+                                setAnswers([]);
+                                navigate(`/done/${surveyId}`);
+                            })
+                            .catch((err) => {
+                                alert('에러가 발생했습니다. 다시 시도해주세요.');
+                                setIsPosting(false);
+                            });
+                     }}
+                     disabled = {isPosting || isBlockToNext}
+                  >
+                    {isPosting ? '제출 중 입니다...' : 제출 }
+                  </Button>) : (
+                  <Button
+                    type="PRIMARY"
+                    onClick={() => {
+                        navigate(`${step+1}`);
+                    }}
+                    disabled = {isBlockToNext}
+                    >
+                        다음
+                    </Button>
+                    )}
     </ActionButtonsWrapper>
-    )
+    );
 }
 
 const ActionButtonsWrapper = styled.div`
@@ -38,6 +74,6 @@ const ActionButtonsWrapper = styled.div`
     justify-content: center;
     display: flex;
     gap: 16px;
-`
+`;
 
 export default ActionButtons;
--- react-study/survey-pie/src/components/ProgressIndicator/index.js
+++ react-study/survey-pie/src/components/ProgressIndicator/index.js
@@ -1,3 +1,11 @@
+import { useRecoilValue } from 'recoil';
+import styled from 'styled-components';
+
+import useAnswers from '../../hooks/useAnswers';
+import useStep from '../../hooks/useStep';
+import questionsLengthState from '../../globalVariable/survey/questionsLengthState';
+import Bar from '../Bar';
+
 function ProgressIndicator() {
     return <></>
 }
